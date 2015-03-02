d707a16c0cefe6d149f372a566f276e0e15d7c7f
Henrique Winckler <henriquew@gmail.com>
2015-03-01 22:41:12 -0300

    Commit inicial e arquivo .gitignore

--- src/tests/ReadTest.java
+++ src/tests/ReadTest.java
@@ -7,7 +7,7 @@ import read.RSSFeedParser;
 public class ReadTest {
 
 	  public static void main(String[] args) {
-		    RSSFeedParser parser = new RSSFeedParser("http://feeds.feedburner.com/bloglecom");
+		    RSSFeedParser parser = new RSSFeedParser("http://www.vogella.com/article.rss");
 		    Feed feed = parser.readFeed();
 		    System.out.println(feed);
 		    for (FeedMessage message : feed.getMessages()) {
