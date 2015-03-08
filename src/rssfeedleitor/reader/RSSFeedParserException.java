f28492ab1beded7026f8c93b4c6b15e5f884f737
Henrique Winckler <henriquew@gmail.com>
2015-03-08 11:44:43 -0300

    Novos ajustes

--- src/rssfeedleitor/reader/RSSFeedParserException.java
+++ src/rssfeedleitor/reader/RSSFeedParserException.java
@@ -2,6 +2,8 @@ package rssfeedleitor.reader;
 
 public class RSSFeedParserException extends Exception {
 
+	private static final long serialVersionUID = 1L;
+
 	public RSSFeedParserException(Exception e) {
 		super(e);
 	}
